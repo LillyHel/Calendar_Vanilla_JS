78c5f70d75f81246639f330fa3dc6e4ef0de3bfe
Lilly Bernhardt <s85269@informatik.htw-dresden.de>
2024-05-27 22:31:29 +0200

    new logo and other small fixes

--- service-worker.js
+++ service-worker.js
@@ -12,32 +12,42 @@ self.addEventListener('install', event => {
       .then(cache => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
+      }).catch(error => {
+        console.error('Failed to cache:', error);
+        for (const url of urlsToCache) {
+          fetch(url).catch(err => console.error('Failed to fetch:', url, err));
+        }
       })
   );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request).then(
-          response => {
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            const responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
+  if (event.request.url.startsWith('http')) {  // Nur http/https Anfragen abfangen
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => {
+          if (response) {
             return response;
           }
-        );
-      })
-  );
+          return fetch(event.request).then(
+            response => {
+              if (!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+              const responseToCache = response.clone();
+              caches.open(CACHE_NAME)
+                .then(cache => {
+                  cache.put(event.request, responseToCache);
+                });
+              return response;
+            }
+          );
+        }).catch(error => {
+          console.error('Failed to fetch resource:', error);
+          return new Response('You are offline and the resource is not cached.');
+        })
+    );
+  }
 });
 
 self.addEventListener('activate', event => {
