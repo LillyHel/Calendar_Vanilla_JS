6c83c78a941044733ae7799f21d9fd36bf454f33
LillyHel <134294713+LillyHel@users.noreply.github.com>
2024-05-27 21:23:50 +0200

    manifest, index and service_worker adjusted

--- service_worker.js
+++ service_worker.js
@@ -1,9 +1,8 @@
 const CACHE_NAME = 'my-site-cache-v1';
 const urlsToCache = [
   '/',
-  '/styles/main.css',
-  '/script/main.js',
-  '/images/logo.png'
+  '/style.css',
+  '/script.js'
 ];
 
 self.addEventListener('install', event => {
